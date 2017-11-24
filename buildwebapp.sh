ee2a1766470149ef8f28e7eaf4c081447a2e1fe6
richard so <richard.cheso@gmail.com>
2017-11-23 21:43:27 -0500

    update build for terraform

--- buildwebapp.sh
+++ buildwebapp.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-echo "SCRIPT running under userid:" 
+echo "SCRIPT running under userid:"
 echo `whoami`
 echo "CURRENT ANT_HOME="$ANT_HOME
 echo "CURRENT PATH="$PATH
 
 
 checkrc() {
- 
+
        echo "------------------------------"
-  echo "$1 return code = " $rc  
+  echo "$1 return code = " $rc
  echo "------------------------------"
   echo " "
        if [ $rc -gt 0 ]; then
           echo "----> $STEP FAILED with return Code: $rc"
-          exit 1 
+          exit 1
         else
-           echo " --> $STEP successful: $rc"   
+           echo " --> $STEP successful: $rc"
         fi
 }
-  
 
-# Check number of arguments passed into script 
+
+# Check number of arguments passed into script
 
 EXPECTED_ARGS=3
 E_BADARGS=12
@@ -42,30 +42,30 @@ echo "current path is: " $curpath
 echo "JDK_PATH is: " $JDK_PATH
 
 cd $rootdir
-echo "updating source from git repository into directory: "$rootdir/$project
-if [  -d "$rootdir/$project" ]; then
+##echo "updating source from git repository into directory: "$rootdir/$project
+##if [  -d "$rootdir/$project" ]; then
    ## remove source directory contents
-   rm -rf $rootdir/$project
-fi
+##   rm -rf $rootdir/$project
+##fi
 
 ##echo `git checkout -- $project`
-echo "git clean -df | git checkout --.  |  git pull "
-echo `git clean -df`
-echo `git checkout -- .`
-echo `git pull`   	   
-echo " --> Finished Git fetch and rebase"
+##echo "git clean -df | git checkout --.  |  git pull "
+##echo `git clean -df`
+##echo `git checkout -- .`
+##echo `git pull`
+##echo " --> Finished Git fetch and rebase"
 
 cd $curpath
 
-chmod 775 -R *
+chmod 775 -R *.sh
 
 if [ ! -d "./wildfly/webapps" ]; then
    mkdir -p ./wildfly/webapps
 fi
 
-chmod 775 -R *
+chmod 775 -R *.sh
 
-ant -f replace_localhost.xml -Ddbhost=mypostgres -Dwebhost=mycentos1 -Dwebport=8080
+ant -f replace_localhost.xml -propertyfile /tmp/build.properties -Ddbhost=mypostgres -Dwebport=8080
 cd $rootdir/$project/
 gradle -Dorg.gradle.java.home=$JDK_PATH build war
 cd $curpath
