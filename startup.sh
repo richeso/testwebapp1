fac62caec176f19a51b556a4e45867a10e8b8600
richard so <richard.cheso@gmail.com>
2017-11-23 23:20:55 -0500

    update java library to default for ubuntu

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ if [ ! -d "./wildfly/webapps" ]; then
    mkdir -p ./wildfly/webapps
 fi
 
-./buildwebapp.sh . testwebapp1 "/c/pg/java/jdk1.7.0_80"
+./buildwebapp.sh . testwebapp1 "/usr/lib/jvm/default-java"
 docker volume create --name postgresdata -d local
 docker-compose up -d
